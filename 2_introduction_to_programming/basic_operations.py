6abcaf385319b8287e1b548dce7b208df7a27eca
mattinpgh <matthew.a.w.smith@gmail.com>
2024-11-08 08:56:03 -0500

    Finish ch 1 and 1st 2 modules of ch 2.

---
+++ 2_introduction_to_programming/basic_operations.py
@@ -0,0 +1,67 @@
+# an_introduction_to_programming/basic_operations.py
+
+
+# * Concatenate two strings, one with your first name and one with your  
+# * last, to create a new string with your full name as its value. For  
+# * example, if your name is John Doe, you should combine 'John' and 'Doe' 
+# * to get 'John Doe'.
+
+first_name = 'Matt'
+last_name = 'Smith'
+full_name = first_name + ' ' + last_name
+
+# * This question may be a little challenging if your math skills are rusty. 
+# * Don't be afraid to take advantage of the hints. Try your best to solve the 
+# * problem, but don't feel compelled to complete it if you become frustrated.
+
+# * Use the REPL and the arithmetic operators to extract the 
+# * individual digits of 4936:
+
+'''One place is 6.
+Tens place is 3.
+Hundreds place is 9.
+Thousands place is 4.'''
+
+ones, tens, hundreds, thousands = 0, 0, 0, 0
+full_num = 4936
+ones = full_num % 10
+full_num = full_num // 10
+tens = full_num % 10
+full_num = full_num // 10
+hundreds = full_num % 10
+full_num = full_num // 10
+thousands = full_num % 10
+full_num = full_num // 10
+
+print(f'1. Ones place is {ones}, \n2. Tens place is {tens}, \n3. Hundreds \
+    place is {hundreds}, \n4. Thousands place is {thousands}.')
+
+# * What does the following code do? Why?
+# print('5' + '10')
+
+'''It concatenates the two strings together to form '510'.'''
+
+# * Refactor the code from the previous exercise to use coercion to print 15
+
+print(int('5') + int('10'))
+
+# * Will an error occur if you try to access a list element with an index 
+# * greater than or equal to the list's length? For example: letters , for
+# * examples: foo = ['a', 'b', 'c'] print(foo[3])      
+
+'''Yes, this will produce an IndexError.'''
+
+# * To what value does the following expression evaluate? 'foo' == 'Foo'
+
+'''False. Strings are case-sensitive.'''
+
+# * What will the following code do? Why? int('3.1415')
+
+'''It will produce an error because only whole numbers can be converted to
+integers. You could instead convert it to a float and // by 1, then convert'''
+
+print(type(int(float('3.1415') // 1))) # returns <class 'int'>
+
+# * To what value does the following expression evaluate? '12' < '9'
+
+'''True. The first character of '12' < the first character of '9'.'''
---
+++ 2_introduction_to_programming/data_types.py
@@ -0,0 +1,31 @@
+# an_introduction_to_python/2_introduction_to_programming/data_types.py
+
+''' Identify the data type or class for each of the following values:
+Value:          Type:
+'True'          str
+False           bool
+(1, 2, 3)       tuple
+1.5             float
+[1, 2, 3]       list
+2               int
+range(5)        range
+{1, 2, 3}       set
+None            NoneType
+{'foo': 'bar'}  dict
+'''
+
+# Create a tuple called names that contains several pet names
+
+pet_names = ('Asta', 'Butterscotch', 'Pudding', 'Neptune', 'Darwin')
+
+# Create a dictionary named pets that containts a list of pet names and the  
+# type of animal 
+
+pet_dict = {
+    'Asta': 'dog',
+    'Butterscotch': 'cat',
+    'Pudding': 'cat',
+    'Neptune': 'fish',
+    'Darwin': 'lizard'
+}
+
